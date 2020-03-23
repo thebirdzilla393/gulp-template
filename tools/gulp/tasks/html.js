dae208d0568dbfda983106800416b9a1e85240b4
nastya <sova393@yandex.ru>
2020-03-23 15:27:52 +0600

    Rename html task and change styles task

--- tools/gulp/tasks/html.js
+++ tools/gulp/tasks/html.js
@@ -1,5 +1,5 @@
 module.exports = function() {
-	$.gulp.task('pug', () => {
+	$.gulp.task('html', () => {
 		return $.gulp.src([
 				$.gulpVariables.dir.src.pages,
 				$.gulpVariables.dir.src.notPageLayout,
@@ -7,20 +7,10 @@ module.exports = function() {
 			])
 			.pipe($.loadPlugins.pug({pretty: true})
 				.on('error', $.loadPlugins.notify.onError(function(error) {
-					return { title: 'Pug', message: error.message };
+					return { title: 'Html', message: error.message };
 				}))
 			)
 			.pipe($.gulp.dest($.gulpVariables.dir.dist.dir_name))
 			.on('end', $.browserSync.reload); // перезагрузка страницы строго после обновления всех страниц
 	});
-	// $.gulp.task('pug', () => {
-	// 	return $.gulp.src($.gulpVariables.dir.src.mainPage)
-	// 		.pipe($.loadPlugins.pug({pretty: true})
-	// 			.on('error', $.loadPlugins.notify.onError(function(error) {
-	// 				return { title: 'Pug', message: error.message };
-	// 			}))
-	// 		)
-	// 		.pipe($.gulp.dest($.gulpVariables.dir.dist.mainPage))
-	// 		.on('end', $.browserSync.reload); // перезагрузка страницы строго после обновления всех страниц
-	// });
 }
--- tools/gulp/tasks/styles.js
+++ tools/gulp/tasks/styles.js
@@ -1,5 +1,6 @@
 module.exports = function () {
-	$.gulp.task('styles', () => {
+	
+	$.gulp.task('styles:css:map', () => {
 		return $.gulp.src($.gulpVariables.dir.src.styles)
 			.pipe($.loadPlugins.sourcemaps.init())
 			.pipe($.loadPlugins.sass({outputStyle: 'expanded'}) //'include css': true,
@@ -10,12 +11,27 @@ module.exports = function () {
 			.pipe($.loadPlugins.autoprefixer({  // префиксы для браузеров
 			 	overrideBrowserslist: ['last 3 versions']
 			}))
-			.pipe($.loadPlugins.sourcemaps.write())
-			.pipe($.gulp.dest($.gulpVariables.dir.dist.styles))
-			.pipe($.loadPlugins.csso()) // минимизация файла (запись в min.css)
-			.pipe($.loadPlugins.sourcemaps.write())
-			.pipe($.loadPlugins.rename({suffix: '.min'}))
+			.pipe($.loadPlugins.sourcemaps.write('.'))
 			.pipe($.gulp.dest($.gulpVariables.dir.dist.styles))
 			.pipe($.browserSync.reload({stream: true})); // обновление без перезагрузки страницы
 	});
+
+	// Задача для записи минимизированной версии файла
+	$.gulp.task('styles:min', () => {
+		return $.gulp.src($.gulpVariables.dir.src.styles)
+			.pipe($.loadPlugins.sass({outputStyle: 'compressed'}) //'include css': true,
+				.on('error', $.loadPlugins.notify.onError(function(error) {
+					return { title: 'Styles', message: error.message };
+				}))
+			)
+			.pipe($.loadPlugins.autoprefixer({  // префиксы для браузеров
+			 	overrideBrowserslist: ['last 3 versions']
+			}))
+			.pipe($.loadPlugins.csso()) // минимизация файла
+			.pipe($.loadPlugins.rename({suffix: '.min'})) // запись в min.css
+			.pipe($.gulp.dest($.gulpVariables.dir.dist.styles))
+			//.pipe($.browserSync.reload({stream: true})); // обновление без перезагрузки страницы
+	});
+
+	$.gulp.task('styles', $.gulp.parallel('styles:css:map', 'styles:min'));
 }
